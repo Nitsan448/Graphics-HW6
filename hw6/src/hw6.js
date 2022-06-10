2a2e609b3289924701f2d193e4f5cfdbbc18c440
Nitsan448 <Nitsan447@gmail.com>
2022-06-10 16:20:43 +0300

    spaceship imported

--- hw6/src/hw6.js
+++ hw6/src/hw6.js
@@ -28,6 +28,11 @@ const texture = loader.load([
 ]);
 scene.background = texture;
 
+const cameraTranslate = new THREE.Matrix4();
+cameraTranslate.makeTranslation(0,0,5);
+camera.applyMatrix4(cameraTranslate)
+
+renderer.render( scene, camera );
 
 // TODO: Texture Loading
 // We usually do the texture loading before we start everything else, as it might take processing time
@@ -39,7 +44,65 @@ scene.background = texture;
 
 // TODO: Spaceship
 // You should copy-paste the spaceship from the previous exercise here
+//Head
+const geometry_head = new THREE.ConeGeometry(1,3,10);
+const material_head = new THREE.MeshBasicMaterial( {color: 0x0ca00a} );
+const Head = new THREE.Mesh(geometry_head,material_head);
 
+const geometryHull = new THREE.CylinderGeometry( 1, 1, 3,10);
+const materialHull = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+const Hull = new THREE.Mesh(geometryHull,materialHull);
+
+const geometryWindow = new THREE.RingGeometry( 2, 3, 32 );
+const materialWindow = new THREE.MeshBasicMaterial( { color: 0xff0000, side: THREE.DoubleSide } );
+const Window = new THREE.Mesh( geometryWindow, materialWindow );
+
+const materialWing = new THREE.MeshBasicMaterial( {color: 0xff0000} );
+materialWing.side= THREE.DoubleSide;
+const geometryWing = new THREE.BufferGeometry();
+const verticesWing = new Float32Array([
+    1, 1, 0,
+    0, -1, 0, // far point of the triangle
+    1, -1, 0
+    ]);
+                                
+geometryWing.setAttribute( 'position', new THREE.BufferAttribute( verticesWing, 3 ) );
+const Wing = new THREE.Mesh(geometryWing, materialWing);
+
+const Spaceship = new THREE.Group();
+Spaceship.name="Spaceship";
+
+const headTranslationY = new THREE.Matrix4().makeTranslation(0,Hull.geometry.parameters.height,0);
+
+const windowZTranslateAmount = Hull.geometry.parameters.height / 5;
+const windowTranslationZ = new THREE.Matrix4().makeTranslation(0,0,Hull.geometry.parameters.radiusBottom+0.001);
+
+const windowScaleUniform = new THREE.Matrix4().makeScale(0.1,0.1,0.1);
+
+const windowTransfromStandard = windowTranslationZ.multiply(windowScaleUniform);
+
+const windowTranslationY = new THREE.Matrix4().makeTranslation(0,windowZTranslateAmount,0);
+const secondWindowTranlationY = new THREE.Matrix4().makeTranslation(0,-windowZTranslateAmount,0);
+
+//Add Wing transformations
+Wing.applyMatrix4(new THREE.Matrix4().makeTranslation(-Hull.geometry.parameters.radiusBottom -0.95  ,0,0))
+Wing.applyMatrix4(new THREE.Matrix4().makeRotationY(degrees_to_radians(30)))
+
+const Wing2 = Wing.clone();
+const Wing3 = Wing.clone();
+Wing2.applyMatrix4(new THREE.Matrix4().makeRotationY(degrees_to_radians(120)));
+Wing3.applyMatrix4(new THREE.Matrix4().makeRotationY(degrees_to_radians(240)))
+Head.applyMatrix4(headTranslationY);
+
+Window.applyMatrix4(windowTransfromStandard);
+const SecondWindow = Window.clone();
+
+Window.applyMatrix4(windowTranslationY);
+SecondWindow.applyMatrix4(secondWindowTranlationY);
+
+Spaceship.add(Head,Hull,Window,SecondWindow,Wing,Wing2,Wing3);
+
+scene.add(Spaceship);
 
 // TODO: Planets
 // You should add both earth and the moon here
@@ -53,9 +116,6 @@ scene.background = texture;
 
 
 // TODO: Add collectible stars
-
-
-
 
 
 // TODO: Add keyboard event
